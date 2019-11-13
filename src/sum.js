220c15b0ce74917fc2e5dc62b08acbcee370d571
Vanya Funez <VFunez@ucvts.tec.nj.us>
2019-11-13 13:22:57 -0500

    one is done

--- src/sum.js
+++ src/sum.js
@@ -1,18 +1,23 @@
 const readlineSync = require("readline-sync");
 
-const userLowerBound = Number(readlineSync.question("\nLower bound: "));
-const userUpperBound = Number(readlineSync.question("Upper bound: "));
+let lowerBound = 1;
+let upperBound = 0;
 
-let lowerBound = userLowerBound;
-let upperBound = userUpperBound;
-while (lowerBound > upperBound) && (lowerBound < Number.MIN_SAFE_INTEGER) && (lowerBound > Number.MAX_SAFE_INTEGER) && (upperBound < Number.MIN_SAFE_INTEGER) && (upperBound > Number.MAX_SAFE_INTEGER);{
+while (lowerBound > upperBound ||
+       lowerBound < Number.MIN_SAFE_INTEGER ||
+       lowerBound > Number.MAX_SAFE_INTEGER ||
+       upperBound < Number.MIN_SAFE_INTEGER ||
+       upperBound > Number.MAX_SAFE_INTEGER) {
+
     lowerBound = Number(readlineSync.question("Lower bound: "));
-    lowerBound = Number(readlineSync.question("Upper bound: "));
-
-    if (lowerBound < upperBound) {
-        let n = (upperBound - lowerBound) + 1
-        let sum = (n / 2)(lowerBound + upperBound)
-    }
+    upperBound = Number(readlineSync.question("Upper bound: "));
 }
 
-console.log("\n" + sum + "\n");
+let sum = 0;
+for (let i = lowerBound; i <= upperBound; i++) {
+  if (i % 2 === 0) {
+    sum = sum + i;
+  }
+}
+
+console.log(`\n${sum.toLocaleString()}.`);
